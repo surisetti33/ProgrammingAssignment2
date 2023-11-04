a9c4fee9919d5fdc65e23fa48adccc01421b8bba
surisetti33 <142832875+surisetti33@users.noreply.github.com>
2023-11-05 02:33:41 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeVector function creates a list with methods to set and get a vector and its mean.
 makeVector <- function(x = numeric()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+    m <- NULL  # Initialize a variable for mean.
+    set <- function(y) {  # Method to set the vector.
+        x <<- y
+        m <<- NULL  # Reset the mean when the vector is updated.
+    }
+    get <- function() x  # Method to get the vector.
+    setmean <- function(mean) m <<- mean  # Method to set the mean.
+    getmean <- function() m  # Method to get the mean.
+    list(set = set, get = get, setmean = setmean, getmean = getmean)  # Return a list of methods.
 }
 
-## Write a short comment describing this function
-
+## cachemean function calculates and caches the mean of a vector.
 cachemean <- function(x, ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
+    m <- x$getmean()  # Attempt to get the cached mean.
+    if (!is.null(m)) {
+        message("getting cached data")  # Display a message if the mean is cached.
+        return(m)  # Return the cached mean.
+    }
+    data <- x$get()  # Get the vector data.
+    m <- mean(data, ...)  # Calculate the mean with optional arguments.
+    x$setmean(m)  # Cache the calculated mean.
+    m  # Return the calculated mean.
 }
